2457dca83e0b2ea0f5eac3cfa1c17d335c6218cd
nafiswatsiq <amrullohnafis@gmail.com>
2024-12-06 02:29:06 +0700

    feat: kelurahan resource

---
+++ app/Filament/Resources/KelurahanResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\KelurahanResource\Pages;
+use App\Filament\Resources\KelurahanResource\RelationManagers;
+use App\Models\Kelurahan;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class KelurahanResource extends Resource
+{
+    protected static ?string $model = Kelurahan::class;
+
+    protected static ?string $navigationGroup = 'Setting';
+    protected static ?string $navigationIcon = 'heroicon-o-home';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nama')
+                    ->required()
+                    ->columnSpanFull()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('longitude')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('latitude')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nama')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('longitude')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('latitude')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageKelurahans::route('/'),
+        ];
+    }
+}
